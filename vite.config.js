538575b8bcfb910caf44c675224d3d12216bd79b
Jana Steinborn <123842012+janasteinborn@users.noreply.github.com>
2025-06-25 20:03:18 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/e-plantShopping/", // must include slashes and match your repo name exactly
+  base: "/e-plantShopping/", // <- must match your GitHub repo name exactly
   plugins: [react()],
 })
